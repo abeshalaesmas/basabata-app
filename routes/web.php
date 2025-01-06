983c18d97e5f2916a95479d1f5aafbf08fc9cf09
Garrell <garrellmacarilay@student.laverdad.edu.ph>
2025-01-06 20:39:09 +0800

    Describe your changes here

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/', function () {
         Route::get('/login',[AuthController::class,'loginForm'])->name('login');
         Route::post('/login',[AuthController::class,'login'])->name('login.process');
     });
-
+        //DASHBOARD
     Route::middleware('auth')->group(function(){
         Route::get('/dashboard',[AuthController::class, 'dashboard'])->name('dashboard');
         Route::post('/logout',[AuthController::class,'logout'])->name('logout');
