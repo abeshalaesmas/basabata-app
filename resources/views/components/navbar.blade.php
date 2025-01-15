c9b1c7dfbcdeb22b3b107f1344c56cbb94b38e7a
Jeremy-Ortega <jeremysortega@gmail.com>
2025-01-15 11:18:34 +0800

    safety commit

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,27 +5,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/siimple-icons/siimple-icons.css" />
     <title>Document</title>
 </head>
 <body>
-    <div class="footer">
+    <div class="navbar">
         <div class="nav-item">
-            <img src="{{ asset('/images/Icons/home.svg') }}" alt="🏠">
-        </div>
-        <div class="nav-item">
-            <img src="{{ asset('/images/Icons/book-icon.svg') }}" alt="📖">
-        </div>
-        <div class="nav-item">
-            <img src="{{ asset('/images/Icons/bulb-icon.svg') }}" alt="💡">
-        </div>
-        
-        <div class="nav-item">
-            <img src="{{ asset('/images/Icons/saved-icon(1).svg') }}" alt="❤️">
-
+            <a href="{{route('profile')}}">
+                {{-- <img src="{{ asset('/images/Icons/home.svg') }}" alt="🏠"> --}}
+                <i class="si-home" style="font-size: 35px"></i>
+            </a>
         </div>
         <div class="nav-item">
             <a href="{{route('profile')}}">
-            <img src="{{ asset('/images/Icons/profile.svg') }}" alt="Me">
+                {{-- <img src="{{ asset('/images/Icons/book-icon.svg') }}" alt="📖"> --}}
+                <i class="si-book" style="font-size: 35px"></i>
+            </a>
+        </div>
+        <div class="nav-item">
+            <a href="{{route('profile')}}">
+                {{-- <img src="{{ asset('/images/Icons/bulb-icon.svg') }}" alt="💡"> --}}
+                <i class="si-lightbulb" style="font-size: 35px"></i>
+            </a>
+        </div>
+        
+        <div class="nav-item">
+            <a href="{{route('profile')}}">
+                {{-- <img src="{{ asset('/images/Icons/saved-icon (1).svg') }}" alt="❤️"> --}}
+                <i class="si-bookmark" style="font-size: 35px"></i>
+            </a>
+        </div>
+        <div class="nav-item">
+            <a href="{{route('profile')}}">
+                {{-- <img src="{{ asset('/images/Icons/profile.svg') }}" alt="Me"> --}}
+                <i class="si-user" style="font-size: 35px"></i>
             </a></div>
     </div>
 </body>
