c95c4adf56c7be3031fb1914a9fb10735fb0048a
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-01-27 09:23:16 +0800

    initialize

---
+++ resources/views/games/quiz/quiz8.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>QUIZ APP</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="user-id" content="{{ auth()->user()->id }}">
+</head>
+<body>
+    <div class="app">
+        <h1>Quiz 1</h1>
+        <div class="quiz">
+            <h2 id="question">Question goes here</h2>
+            <div id="answer-buttons">
+                <!-- Remove initial buttons to avoid conflicts -->
+            </div>
+            <button id="revealButton" style="display: none;">Reveal Answer</button>
+            <button id="next-btn">Next</button>
+        </div>
+    </div>
+    <script src="{{asset('js/quizzes/quiz-8.js')}}"></script>
+</body>
+</html>
