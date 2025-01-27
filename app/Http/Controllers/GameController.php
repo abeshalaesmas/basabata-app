cfbc49317b458ee58f9aad8588632f45f2dbf9fa
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-01-27 09:28:15 +0800

    hshs

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -15,25 +15,21 @@ class GameController extends Controller
 
 
     //QUIZ
-    public function showQuiz(){ return view('quiz.show_quiz');}
+    public function showQuiz(){ return view('games.quiz.show_quiz');}
 
-    public function quizOne() { return view('quiz.quiz1'); }
-    public function quizTwo() { return view('quiz.quiz2'); }
-    public function quizThree() { return view('quiz.quiz3'); }
-    public function quizFour() { return view('quiz.quiz4'); }
-    public function quizFive() { return view('quiz.quiz5'); }
-    public function quizSix() { return view('quiz.quiz6'); }
-    public function quizSeven() { return view('quiz.quiz7'); }
-    public function quizEight() { return view('quiz.quiz8'); }
-    public function quizNine() { return view('quiz.quiz9'); }
-    public function quizTen() { return view('quiz.quiz10'); }
-    public function quizEleven() { return view('quiz.quiz11'); }
-    public function quizTwelve() { return view('quiz.quiz12'); }
-    public function quizThirteen() { return view('quiz.quiz13'); }
-    public function quizFourteen() { return view('quiz.quiz14'); }
-    public function quizFifteen() { return view('quiz.quiz15'); }
-
-
-
-   
+    public function quizOne() { return view('games.quiz.quiz1'); }
+    public function quizTwo() { return view('games.quiz.quiz2'); }
+    public function quizThree() { return view('games.quiz.quiz3'); }
+    public function quizFour() { return view('games.quiz.quiz4'); }
+    public function quizFive() { return view('games.quiz.quiz5'); }
+    public function quizSix() { return view('gaems.quiz.quiz6'); }
+    public function quizSeven() { return view('games.quiz.quiz7'); }
+    public function quizEight() { return view('games.quiz.quiz8'); }
+    public function quizNine() { return view('games.quiz.quiz9'); }
+    public function quizTen() { return view('games.quiz.quiz10'); }
+    public function quizEleven() { return view('games.quiz.quiz11'); }
+    public function quizTwelve() { return view('games.quiz.quiz12'); }
+    public function quizThirteen() { return view('games.quiz.quiz13'); }
+    public function quizFourteen() { return view('games.quiz.quiz14'); }
+    public function quizFifteen() { return view('games.quiz.quiz15'); }
 }
