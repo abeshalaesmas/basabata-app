1a60aaf2f1cd0206330940f3dd06abb237dba0f4
j4melaF <jamela.fernandez.29@gmail.com>
2025-01-27 14:17:33 +0800

    Kabanata 1-13

---
+++ resources/views/chapters/chapter-8.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body {
+            margin: 50px; /* Adjust the margin size as needed */
+            font-family: 'Times New Roman', Times, serif;
+            line-height: 1.6;
+        }
+
+        header {
+            background-color: #f3f4f6;
+            padding: 1rem;
+            text-align: center;
+        }
+
+        main {
+            display: flex;
+            justify-content: space-between;
+            gap: 1rem;
+            padding: 1rem;
+            margin-bottom: 40px;
+        }
+
+        p {
+            text-align: justify;
+            text-indent: 30px;
+            white-space: pre-wrap;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>KABANATA 8: MABUTING PASKÓ</h1>
+    </header>
+
+    <main>
+        <p>
+            Nang imulat ni Hulî ang namumuktông matá ay nákitang madilím pá ang bahay. Nagtitilaok ang mg̃a manók. Ang unang pumasok sa kaniyáng kalooban ay ang sapantahàng marahil ay gumawâ ng̃ isáng himalâ ang Birhen at ang araw ay hindî sisilang kahi’t na inaanyayahan ng̃ mg̃a manók.
+
+            [71]Nagbang̃on siyá, nag angtanda, taimtím na dinasál ang kaniyáng mg̃a dalang̃in sa umaga at lumabás sa batalán, na pinag-iing̃atang huwag may kumalatís sa kaniyáng paglakad.
+
+            Walâng himalâ; ang araw ay sisikat na, ang umaga’y magiging maliwanag, ang simuy ay may daláng lamíg, ang mg̃a bituwín sa silang̃anan ay lumálamlám at ang mg̃a manók ay naglalalò sa kaniláng pagtitilaok. Ang gayó’y malabis na paghing̃î; madalî pang magawâ ng̃ Birhen ang magpadalá ng̃ dalawáng daa’t limang pûng piso! ¿Anó na lamang sa kaniyá, siyá na Iná ng̃ Dios, ang magbigáy noon? Ng̃unì’t sa ilalim ng̃ larawan ay walâng nátagpûán kundî ang sulat ng̃ kaniyáng amá na humihíng̃î ng̃ limáng daang pisong pangtubós.... Walâ ng̃ daan kundî ang lumakad. Nang mákitang ang kaniyáng lelong ay hindî kumikilos, ay inakalàng natutulog, at naglutò na siyá ng̃ salabát na pang-agahan. ¡Katakátaká! Siyá’y panatag at warì pang ibig mátawá. ¿Anó kayâ ang dinamdám niyá’t naghinagpís ng̃ katakottakot ng̃ gabíng yaón? Hindî namán siyá málalayô, maaarìng dalawin sa tuwîng makalawá ang bahay; mákikita siyá ng̃ kaniyáng lelong, at tungkol namán kay Basilio ay malaon ng̃ alám ang masamâng lakad ng̃ usap ng̃ kaniyáng amá, kayâ’t madalás na sabihin sa kaniyà na:
+
+            —Pag akó’y nagíng médiko at mákasal na tayo ay hindî na kakailang̃anin ng̃ amá mo ang kaniyáng mg̃a lupàín.
+
+            —¡Malakíng kahang̃alán ang nágawâ kong panggigipuspós!—anyá sa sarili samantalang inaayos ang kaniyáng tampipì.
+
+            At sa dahiláng násagì ng̃ kaniyáng mg̃a dalirì ang agnós ay inilapit sa kaniyáng labì, hinagkán, ng̃unì’t kinuskos kaagad ang bibíg dahil sa takot na máhawa; ang agnós na iyon na may brillante at esmeralda ay galing sa isáng ketong̃in.... ¡Ah! kung magkagayón, kung siyá’y magkaroon ng̃ gayóng sakít, ay hindî na siyá mag-aasawa.
+
+            Sa dahiláng naglíliwanág na at nákita ang kaniyáng lelong na nakaupô sa isáng sulok, na sinusundán ng̃ ting̃ín ang lahát ng̃ kaniyáng kilos, ay kinuha ang kaniyáng tampipì ng̃ damít at nakang̃itîng lumapit upang humalík ng̃ kamáy. Benindisionan siyá ng̃ matandâ na walâng kaimíkimík. Nagbirô pá siyá.
+
+            —Pagdatíng ni tatay ay sabihin ninyóng nápasok din [72]akó sa kolehiyo: ang pang̃inoon ko’y marunong ng̃ kastilà. Itó ang mura sa lahát ng̃ kolehiyong mákikita.
+
+            At ng̃ mákitang napupunô ng̃ luhà ang mg̃a matá ng̃ matandâ ay sinunong ang tampipì at matuling pumanaog sa hagdanan. Ang kaniyáng sinelas ay masayáng tumutunóg sa mg̃a baitang na kahoy.
+
+            Ng̃unì’t ng̃ luming̃ón upang tumanáw pang mulî sa kaniyáng bahay, ang bahay na kinapawìan ng̃ kaniyáng kabatàan, at kinasilang̃an ng̃ kaniyáng mg̃a unang pang̃arap sa pagkadalaga; ng̃ mákita niyáng malungkót, nagiísá, walâng tao, sa mg̃a durung̃awáng nakalapat ng̃ kauntì ay walâng nakadung̃aw at madilím na gaya ng̃ mg̃a matá ng̃ isáng patáy; ng̃ mading̃íg ang mahinàng kaluskós ng̃ kakawayanan at nákitang nang̃agduduyan dahil sa simuy sa umaga na warìng nagsasabi sa kaniyáng “paalam”, ay nawalâ ang kaniyáng maliksíng kilos, nápahintô siyá, ang kaniyáng mg̃a matá’y napunô ng̃ luhà at matapos magpatiupô sa isáng sang̃á ng̃ kahoy na nasa tabíng daan ay umiyák ng̃ kahapishapis.
+
+            Malaon nang nakaalís si Hulî at mataás ng̃ lubhâ ang araw. Si tandâng Selo ay nakadung̃aw at tinátanáw ang mg̃a nakagayák na taong tung̃o sa bayan upang magsimbá sa misa mayor. Halos lahát ay may dalá ó kayâ’y kilik na batàng lalaki ó batàng babai na nabibihisang warì’y tung̃o sa isáng kapistahan.
+
+            Ang kaarawán ng̃ Paskó sa Pilipinas, ayon sa matatandâ, ay pistá ng̃ mg̃a batà; marahil ay hindî kasang-ayon sa gayóng akalà ang mg̃a batà, at mahihinalàng kanilá pang kinatatakutan ang araw na iyón. At gayón ng̃â marahil, sapagkâ’t ginigising siláng maaga, linilinis, binibihisan at isinusuót sa kanilá ang lahát ng̃ kagayakang bago, mahal at mainam na mayroon silá, mg̃a sapatos na sutlâ, malalakí’t malalapad na sombrero, damít na lana, sutlâ ó tersiopelo, kasama ang apat ó limáng maliliít na kalmen na may daláng ebanhelio ni San Juán, at matapos na matagláy ang lahát ng̃ yaón ay dinadalá silá sa misa mayor na kulángkuláng sa isáng oras ang habà, pinagtitiís silá ng̃ init at sing̃áw ng̃ maraming taong nagkakasiksikan at nagpapawís, at kung hindî man silá pinagdadasál ng̃ rosario ay kailang̃an namán nilá ang huwag maglilikót, mayamót ó mákatulog. Sa bawà’t kagaslawáng makapagpapadumí [73]sa damít ay isáng kurót ó isáng galit ang katugón; kayâ’t hindî man silá tumatawa ni naggagalák at nábabasa sa kaniláng mg̃a matá ang pag-aalaala sa lumàng kamisola na pang-araw-araw at ang pag tutol sa tungkol may burdá. Matapos yaón ay dinádalá silá sa mg̃a baháy baháy ng̃ mg̃a kamag-anakan upang humalík ng̃ kamáy; doon ay kailang̃an nilá ang magsayáw, umawit at sabihin ang lahát ng̃ ikatutuwâng nálalaman, sa ibig man ó sa ayaw, suot ó hindî man ang kaniláng masisikíp na bihis, na kasama rin ang kurót at galit pag sumuway ó gumawâ noong bagay na dî iniuutos sa kanilá. Binibigyán silá ng̃ kualta ng̃ mg̃a kamag-anak, ng̃unì’t karaniwang hindî man nilá másilip pagkatapos, sapagkâ’t kinukuha ng̃ kaniláng mg̃a magulang. Ang tang̃ìng bagay na kaniláng nápapalâ sa mg̃a kapistahang yaón ay ang mg̃a bakás ng̃ kurót na tinuran, ang mg̃a kasikipán at madalás pa’y isáng pagkasirà ng̃ sikmurà dahil sa pagkabundát sa matamís ó biskotso sa bahay ng̃ mg̃a kamag-anak. Datapwâ’y yaón ang kaugalìan at ang mg̃a batà’y pumasok sa kabuhayan sa gayóng paraan, na sa isáng dako’y siyáng pinaká hindî malungkot, ang pinaká lalòng hindî mahirap sa kabuhayan ng̃ mg̃a taong yaón.
+
+            Ang mg̃a taong may kagulang̃an na, may sarili nang pamumuhay, ay nákakalahók din sa kapistahang itó. Dumadalaw sa kaniláng mg̃a magulang at mg̃a minamà, iluluhód ang isáng paa at babatìín ng̃ magandáng paskó: ang kaniláng aginaldo ay isáng matamís, isáng bung̃ang kahoy, isáng basong tubig ó isáng muntîng handóg na walâng halagá.
+
+            Napagmamasdán ni tandâng Selo ang pagdaraan ng̃ kaniyáng mg̃a kaibigan at malungkót na iniisip na walâ siyáng maibibigáy na aginaldo sa kang̃ino man ng̃ taóng iyón at ang kaniyáng apó ay umalís na walâng tagláy na aginaldo at hindî man lamang siyá nábatì ng̃ magandáng paskó. ¿Isá kayâng kahinhinán iyón ni Hulî ó isáng pagkalimot lamang?
+
+            Nang sinalubong ni tandâng Selo ang mg̃a kamag-anakang dumatíng na dalá ang kaníkaniláng mg̃a anák ay hindî makabigkás ng̃ anománg salitâ, kayâ’t sa sarili niyá’y námanghâ; walâng nangyari sa pagpupumilit, hindî nakabanggít ng̃ isá mang sabi. Pinigilan ang kaniyáng lalamunan, pinailíng [74]ang ulo, hindî rin maarì! ¡nagnasàng tumawa at ang kaniyáng mg̃a labì ay kumimbót lamang! ang tang̃ìng napalabás sa kaniyáng bibíg ay isáng ipít na tunóg na kagaya ng̃ sa hungkóy. Gulilát na nang̃agkating̃inan ang mg̃a babai.
+
+            —¡Pipí, pipí!—ang sigawang balót ng̃ sindák at nang̃agkaguló.
+        </p>
+    </main>
+</body>
+</html>
